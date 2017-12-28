0be82e6a475f8f79e6b4a6956680911d08d743c4
anantlk <anant.lk.1997@gmail.com>
2017-12-29 00:20:35 +0530

    Added constraint for max 2 duties per day

--- desk_duty.py
+++ desk_duty.py
@@ -1,4 +1,5 @@
 import xlrd
+import xlwt
 import random
 from random import choice,sample
 from os.path import join,dirname,abspath
@@ -12,6 +13,13 @@ workSheet=workBook.sheet_by_name("Sheet1")
 numCol=workSheet.ncols
 numRow=workSheet.nrows
 
+def check(per,count):
+	for name in per:
+		if(count[name]==2):
+			return 0
+		else:
+			count[name]+=1;
+	return 1
 for i in range(1,numCol):
 	schd[i]=[]
 for i in range(2,numRow):
@@ -26,13 +34,20 @@ for i in range(2,numRow):
 for slot in schd:
 	per1=random.sample(schd[slot],2)
 	per2=random.sample(list(set(schd[slot])-set(per1)),2)
+	while(check(per1,count)!=1):
+		per1=random.sample(schd[slot],2)
+		print("hello")
 	table["sjt"][slot]=per1
+	while(check(per2,count)!=1):
+		per2=random.sample(list(set(schd[slot])-set(per1)),2)
+		print("hello")
 	table["tt"][slot]=per2
 
 
 
-print(schd)
-print("\n\n\n")
+#print(schd)
+#print("\n\n\n")
 print(table)
 print("\n\n\n")
 print(count)
+
